abaadf857b080c87d2f8e29ec004f65c3707ed58
Frank S. Thomas <frank@timepit.eu>
2016-02-13 16:54:16 +0100

    Remove explicit dependency on scalastyle plugin

--- project/MdocLibrary.scala
+++ project/MdocLibrary.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object MdocLibrary {
-  val fshell = "org.m-doc" %% "fshell" % "0.0.0-36-gcdc793d"
+  val fshell = "org.m-doc" %% "fshell" % "0.0.0-38-gf528291"
   val renderingEngines = "org.m-doc" %% "rendering-engines" % "0.0.0-17-g088fa18"
-  val commonModel = "org.m-doc" %% "common-model" % "0.0.0-9-g30b5e8b"
+  val commonModel = "org.m-doc" %% "common-model" % "0.0.0-10-g73865cc"
 }
